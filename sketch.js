8a222f17e2c3894fc63baed6a1b6c3cb4739a337
Arjun Bhobe <arjun@bhobe.com>
2023-02-22 11:14:54 -0800

    Redefine functions to follow math definitions

--- sketch.js
+++ sketch.js
@@ -7,7 +7,12 @@ let func = p =>  {
 		p.createCanvas(400, 400);
 		p.background(p.color('white'));
 		p.stroke(255);
-		
+
+		for (let i=0; i<p.width; i++) {
+			p.stroke(p.color('black'));
+			p.line(i,o(f(s(i))),i-1,o(f(s(i-1))));
+			console.log('i',i, 'o(f(s(i)))', o(f(s(i))))
+		} 
 		
 		checkbox = p.createCheckbox('Pause', false);
 		checkbox.position(500,500)
@@ -19,10 +24,13 @@ let func = p =>  {
         p.noStroke();
 		for (let i=0; i<p.width; i++) {
 			p.stroke(p.color('black'));
-			p.line(i,off(f, i),i-1,off(f,i-1));
-		}    
-		p.ellipse(j, off(f,j), 5, 5);
-		p.line(j, off(f,j), 0, off(g,j));
+			p.line(i,o(f(s(i))),i-1,o(f(s(i-1))));
+			
+		} 
+		   
+		p.ellipse(j, o(f(s(j))), 5, 5);
+		p.line(0, o(t(s(0))), p.width, o(f(s(p.width))));
+		//console.log('j:', j, 'o(t(0)):', )
 		if (!checkbox.checked()) { 
 			j+=1;
 			if (j>=p.width) {
@@ -32,21 +40,30 @@ let func = p =>  {
 		}
 	}
 
-	function off(inp, x) {
-		return p.height - (inp((x-p.width/2)/scale) + p.height/2)
+	// Flip and y-offset
+	function o(x) {
+		return p.height - x - p.height/2
 	}
 
+	// Scale and x-offser
+	function s(x) {
+		return (x-p.width/2)/scale
+	}
+
+	//Function
 	function f(x){
 		return x**3+2*x**2;       
 	}
 
-	function fpr(x){
-		return 3*x**2+4*x;   
+	//Derivative
+	function d(x){
+		return 3*(x**2)+4*x;   
 		     
 	}
 
-	function g(x) {
-		return fpr(x)*(0-x)+f(x)
+	//Tangent Generator
+	function t(x) {
+		return d(j)*(x-j)+f(j)
 	}
 }
 
@@ -89,5 +106,5 @@ let deriv = p =>  {
 }
 
 new p5(func, 'function');
-new p5(deriv, 'derivative')
+//new p5(deriv, 'derivative')
 
